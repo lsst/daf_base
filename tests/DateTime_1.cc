5f9b513226e7b857dc709316ead9bb29bec8d3c5
ktlim <ktlim@git.lsstcorp.org>
2009-05-18 18:30:56 +0000

    64-bit machines can process wild dates that 32-bit machines cannot.

--- tests/DateTime_1.cc
+++ tests/DateTime_1.cc
@@ -48,16 +48,21 @@ BOOST_AUTO_TEST_CASE(Throw) {
     // Date before UTC->TAI conversion is valid
     BOOST_CHECK_THROW(DateTime(-500000000 * 1000000000LL, DateTime::UTC),
                       lsst::pex::exceptions::DomainErrorException);
-    // Date too far in the past for Unix mktime()
-    BOOST_CHECK_THROW(DateTime(1901, 1, 1, 12, 34, 56),
-                      lsst::pex::exceptions::DomainErrorException);
+    // Date before UTC->TAI conversion is valid and too far in the past for
+    // 32-bit Unix mktime()
     BOOST_CHECK_THROW(DateTime("1901-01-01T12:34:56Z"),
                       lsst::pex::exceptions::DomainErrorException);
-    // Date too far in the future for Unix mktime()
-    BOOST_CHECK_THROW(DateTime(2039, 1, 1, 12, 34, 56),
-                      lsst::pex::exceptions::DomainErrorException);
-    BOOST_CHECK_THROW(DateTime("2039-01-01T12:34:56Z"),
-                      lsst::pex::exceptions::DomainErrorException);
+    if (sizeof(time_t) == 4) {
+        // Date too far in the past for Unix mktime()
+        BOOST_CHECK_THROW(DateTime(1901, 1, 1, 12, 34, 56),
+                          lsst::pex::exceptions::DomainErrorException);
+        // Date too far in the future for Unix mktime()
+        BOOST_CHECK_THROW(DateTime(2039, 1, 1, 12, 34, 56),
+                          lsst::pex::exceptions::DomainErrorException);
+        BOOST_CHECK_THROW(DateTime("2039-01-01T12:34:56Z"),
+                          lsst::pex::exceptions::DomainErrorException);
+    }
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
