1a41ab7c6e390adefe2b3625b0fad6d83333d95b
smm <smm@git.lsstcorp.org>
2010-04-08 01:14:21 +0000

    moving daf_base tag out of trunk to tags

--- 3.2.10/include/lsst/daf/base.h
+++
@@ -1,9 +0,0 @@
-#ifndef LSST_DAF_BASE_H
-#define LSST_DAF_BASE_H
-
-#include "lsst/daf/base/Citizen.h"
-#include "lsst/daf/base/DateTime.h"
-#include "lsst/daf/base/Persistable.h"
-#include "lsst/daf/base/PropertySet.h"
-
-#endif
