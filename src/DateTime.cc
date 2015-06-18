1d3dfe586afa294689afaafddf19826fcb16955f
Tim Jenness <tjenness@lsst.org>
2015-06-18 13:46:39 -0700

    Add the 2015 leap second

--- src/DateTime.cc
+++ src/DateTime.cc
@@ -124,6 +124,7 @@ static std::string leapString =
 2006 JAN  1 =JD 2453736.5  TAI-UTC=  33.0       S + (MJD - 41317.) X 0.0      S\n\
 2009 JAN  1 =JD 2454832.5  TAI-UTC=  34.0       S + (MJD - 41317.) X 0.0      S\n\
 2012 JUL  1 =JD 2456109.5  TAI-UTC=  35.0       S + (MJD - 41317.) X 0.0      S\n\
+2015 JUL  1 =JD 2457204.5  TAI-UTC=  36.0       S + (MJD - 41317.) X 0.0      S\n\
 ";
 
 // Anonymous namespace for structures, classes, and formerly file-static
