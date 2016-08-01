f1b491e0cad2fed8781d2896cb9db3187928ff6d
Pim Schellart <P.Schellart@princeton.edu>
2016-08-01 16:46:55 -0400

    Wrap with pybind11 instead of swig

--- python/lsst/daf/base/__init__.py
+++ python/lsst/daf/base/__init__.py
@@ -1,6 +1,7 @@
 # 
 # LSST Data Management System
-# Copyright 2008, 2009, 2010 LSST Corporation.
+#
+# Copyright 2008-2016  AURA/LSST.
 # 
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
@@ -17,8 +18,10 @@
 # 
 # You should have received a copy of the LSST License Statement and 
 # the GNU General Public License along with this program.  If not, 
-# see <http://www.lsstcorp.org/LegalNotices/>.
+# see <https://www.lsstcorp.org/LegalNotices/>.
 #
 
-from baseLib import *
+from __future__ import absolute_import
+
 from .version import *
+from .baseLib import *
