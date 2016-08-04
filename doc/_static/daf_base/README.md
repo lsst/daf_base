c024b1c8ae5cbb01a4aaf53d0ed5c817826b05c0
Jonathan Sick <jsick@lsst.org>
2016-08-04 15:21:05 -0700

    Add base Sphinx framework
    Includes breathe+doxygen configuration.

---
+++ doc/_static/daf_base/README.md
@@ -0,0 +1,7 @@
+Place static files for `daf_base` docs, such as images and sample data, in this directory.
+
+They can be referenced in reStructuredText using absolute paths, e.g.:
+
+```
+.. image:: /_static/daf_base/image.jpg
+```
---
+++ doc/conf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""Sphinx configurations to build package documentation."""
+
+import os
+
+from documenteer.sphinxconfig.stackconf import build_package_configs
+
+import lsst.daf.base
+
+
+_g = globals()
+_g.update(build_package_configs(
+    project_name='daf_base',
+    copyright='2016 Association of Universities for '
+              'Research in Astronomy, Inc.',
+    version=lsst.daf.base.version.__version__,
+    doxygen_xml_dirname=os.path.join(os.path.dirname(__file__), 'xml')))
