553f68b2db65a7fe4078fea2603c5a3080a867e7
Pim Schellart <P.Schellart@princeton.edu>
2016-09-23 10:14:22 -0400

    Cleanup after rebase on Python 3 and pytest changes

--- python/lsst/daf/base/dateTime.py
+++ python/lsst/daf/base/dateTime.py
@@ -1,6 +1,8 @@
-from _dateTime import DateTime
+from __future__ import absolute_import
 
-def toPython(self, timescale=None):
+from ._dateTime import DateTime
+
+def DateTime_toPython(self, timescale=None):
     """Convert a DateTime to Python's datetime
 
     @param timescale  Timescale for resultant datetime
@@ -8,5 +10,11 @@ def toPython(self, timescale=None):
     import datetime
     nsecs = self.nsecs(timescale) if timescale is not None else self.nsecs()
     return datetime.datetime.utcfromtimestamp(nsecs/10**9)
+DateTime.toPython = DateTime_toPython
+del DateTime_toPython
 
-DateTime.toPython = toPython
+def DateTime__repr__(self):
+    return "DateTime(\"{}\", TAI)".format(self.toString(DateTime.TAI))
+DateTime.__repr__ = DateTime__repr__
+del DateTime__repr__
+
