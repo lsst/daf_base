9a29c935ed473c94f4bc43be355d7393a8f3d6d0
Pim Schellart <P.Schellart@princeton.edu>
2016-10-11 14:02:23 -0400

    Fix DateTime after rebase

--- python/lsst/daf/base/dateTime.cc
+++ python/lsst/daf/base/dateTime.cc
@@ -7,6 +7,9 @@ using namespace lsst::daf::base;
 
 namespace py = pybind11;
 
+// invalid_nsecs is odr used but has an in-class initializer
+constexpr long long DateTime::invalid_nsecs;
+
 PYBIND11_PLUGIN(_dateTime) {
     py::module mod("_dateTime", "Access to the classes from the daf_base dateTime library");
 
@@ -24,10 +27,12 @@ PYBIND11_PLUGIN(_dateTime) {
         .value("EPOCH", DateTime::DateSystem::EPOCH)
         .export_values();
 
-    cls.def(py::init<long long, DateTime::Timescale>(),
-            py::arg("nsecs") = 0LL, py::arg("scale") = DateTime::Timescale::TAI)
-        .def(py::init<long long, DateTime::Timescale>())
-        .def(py::init<double, DateTime::DateSystem, DateTime::Timescale>())
+    cls.def(py::init<>())
+        .def_readonly_static("invalid_nsecs", &DateTime::invalid_nsecs)
+        .def(py::init<long long, DateTime::Timescale>(),
+             py::arg("nsecs"), py::arg("scale") = DateTime::Timescale::TAI)
+        .def(py::init<double, DateTime::DateSystem, DateTime::Timescale>(),
+             py::arg("date"), py::arg("system")=DateTime::DateSystem::MJD, py::arg("scale")=DateTime::Timescale::TAI)
         .def(py::init<int, int, int, int, int, int, DateTime::Timescale>())
         .def(py::init<const std::string &, DateTime::Timescale>())
         .def("nsecs", &DateTime::nsecs, py::arg("scale") = DateTime::Timescale::TAI)
@@ -38,6 +43,7 @@ PYBIND11_PLUGIN(_dateTime) {
         .def("gmtime", &DateTime::gmtime)
         .def("timespec", &DateTime::timespec)
         .def("timeval", &DateTime::timeval)
+        .def("isValid", &DateTime::isValid)
         .def_static("now", &DateTime::now)
         .def_static("initializeLeapSeconds", &DateTime::initializeLeapSeconds)
         .def(py::self == py::self)
