371fc94ecb16e4ddf1dac8516f6071e8983804b2
Russell Owen <rowen@uw.edu>
2016-12-02 20:08:44 -0800

    Wrap Citizen.getId
    Add a Python unit test for Citizen so getId is tested.

--- python/lsst/daf/base/citizen.cc
+++ python/lsst/daf/base/citizen.cc
@@ -15,6 +15,7 @@ PYBIND11_PLUGIN(_citizen) {
     cls.def_static("census", (int (*)(int, Citizen::memId)) &Citizen::census);
     /* It is strange to expose cout to Python, so instead make a new wrapper */
     cls.def_static("census", [](Citizen::memId id) { Citizen::census(std::cout, id); });
+    cls.def("getId", &Citizen::getId);
     cls.def_static("getNextMemId", &Citizen::getNextMemId);
     cls.def_static("setNewCallbackId", &Citizen::setNewCallbackId);
     cls.def_static("setDeleteCallbackId", &Citizen::setDeleteCallbackId);
