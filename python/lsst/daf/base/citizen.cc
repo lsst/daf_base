956e49d700bed4c37ceaebd1f4454937c2aa7670
Krzysztof Findeisen <kfindeis@uw.edu>
2017-01-09 13:58:57 -0800

    Wrap remaining overload of Citizen::census.

--- python/lsst/daf/base/citizen.cc
+++ python/lsst/daf/base/citizen.cc
@@ -15,6 +15,10 @@ PYBIND11_PLUGIN(_citizen) {
     cls.def_static("census", (int (*)(int, Citizen::memId)) &Citizen::census);
     /* It is strange to expose cout to Python, so instead make a new wrapper */
     cls.def_static("census", [](Citizen::memId id) { Citizen::census(std::cout, id); });
+    // TODO: pybind11 can't handle an owning pointer to a container of non-owning pointers
+    // ::reference is a temporary workaround until we figure out a better way
+    cls.def_static("census", (std::vector<Citizen const *> const * (*)()) &Citizen::census,
+        py::return_value_policy::reference);
     cls.def("getId", &Citizen::getId);
     cls.def("markPersistent", &Citizen::markPersistent);
     cls.def_static("getNextMemId", &Citizen::getNextMemId);
