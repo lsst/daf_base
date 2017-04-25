f3a0b22296bd8a32203466c3c1a54364540f3ab5
Russell Owen <rowen@uw.edu>
2017-04-24 17:00:49 -0700

    Standardize python initial boilerplate
    including adding it in a few cases where it was missing.

--- python/lsst/daf/base/citizen/citizenContinued.py
+++ python/lsst/daf/base/citizen/citizenContinued.py
@@ -1,7 +1,7 @@
 #
 # LSST Data Management System
 #
-# Copyright 2008-2016  AURA/LSST.
+# Copyright 2008-2017  AURA/LSST.
 #
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
@@ -18,7 +18,7 @@
 #
 # You should have received a copy of the LSST License Statement and
 # the GNU General Public License along with this program.  If not,
-# see <https://www.lsstcorp.org/LegalNotices/>.
+# see <http://www.lsstcorp.org/LegalNotices/>.
 #
 
 from __future__ import absolute_import, division, print_function
